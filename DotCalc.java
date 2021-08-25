aa96fb7e81e6d2d3d6627a4cf858d2d3bd701c3b
olanrewaju ayanjoke <o.ayanjoke@tu-bs.de>
2021-08-25 17:00:14 +0200

    Added Multiplication and Division features

--- DotCalc.java
+++ DotCalc.java
@@ -7,7 +7,7 @@ public class DotCalc {
 
     public static void main(String[] args) {
         System.out.println("Enter the operation you wish to apply on both numbers");
-        System.out.println("Operations: '+' (add), '-' (sub)");
+        System.out.println("Operations: '+' (add), '-' (sub), '*' (mult), '/' (div)");
         String operand = sc.nextLine();
         processOperation(operand);
     }
@@ -20,11 +20,23 @@ public class DotCalc {
         return number1 - number2;
     }
 
+    public static int operationMultiply() {
+        return number1 * number2;
+    }
+
+    public static int operationDivide() {
+        return number1 / number2;
+    }
+
     public static void processOperation(String operand) {
         if (operand.equals("+")) {
             printResult(operationAdd());
         } else if (operand.equals("-")) {
             printResult(operationSubstract());
+        } else if (operand.equals("*")) {
+            printResult(operationMultiply());
+        } else if (operand.equals("/")) {
+            printResult(operationDivide());
         } else {
             System.out.println("Invalid Operation");
         }
